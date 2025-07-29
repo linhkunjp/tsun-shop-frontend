dff19e785c8b62faa43e472204afd23a1f40ae55
linhkunjp <ktzjin@gmail.com>
2025-07-30 00:05:49 +0700

    update: toolbar component

--- src/components/ui/toolbar/ToolbarMobile.vue
+++ src/components/ui/toolbar/ToolbarMobile.vue
@@ -1,9 +1,9 @@
 <template>
   <div
     v-if="!isDesktop"
-    class="fixed bottom-0 left-0 right-0 z-999 h-[45px] w-full border-t border-t-[#e7e7e7] bg-white px-2.5 py-[5px]"
+    class="fixed bottom-0 left-0 right-0 z-10 h-[45px] w-full border-t border-t-[#e7e7e7] bg-white px-2.5 py-[5px]"
   >
-    <div class="h-full text-black flex items-center justify-between">
+    <div class="flex items-center justify-between h-full text-black">
       <button><i class="fa-solid fa-phone"></i></button>
       <button><i class="fa-solid fa-envelope"></i></button>
       <button><i class="fa-solid fa-location-dot"></i></button>
